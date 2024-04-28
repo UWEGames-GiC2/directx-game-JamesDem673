73cb650355b2f22d94848a00987388c42fb082f5
JamesDem673 <james2.dempsey@live.uwe.ac.uk>
2024-04-28 09:21:46 +0100

    Removed Redundancy

--- Game/ObjectList.h
+++ Game/ObjectList.h
@@ -8,7 +8,6 @@
 #include "camera.h"
 #include "TPSCamera.h"
 #include "CMOGO.h"
-#include "Tree.h"
 #include "Terrain.h"
 #include "Player.h"
 #include "Monster.h"
@@ -22,12 +21,6 @@
 
 //VBGO examples
 #include "fileVBGO.h"
-#include "VBCube.h"
-#include "VBSpiked.h"
-#include "VBspiral.h"
-#include "VBPillow.h"
-#include "VBSnail.h"
-#include "MarchCubes.h"
 
 //2D Game Objects
 #include "GameObject2D.h"
